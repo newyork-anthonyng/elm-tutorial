57b4df10506679fab47e396c66f4552af5375351
Anthony Ng <newyork.anthonyng@gmail.com>
2018-02-09 21:44:24 -0800

    Add Player List view

--- src/Models.elm
+++ src/Models.elm
@@ -1,4 +1,18 @@
 module Models exposing (..)
 
-type alias Model
-  = String
+type alias Model =
+  { players : List Player
+  }
+
+initialModel : Model
+initialModel =
+  { players = [ Player "1" "Sam" 1 ]
+  }
+
+type alias PlayerId = String
+
+type alias Player =
+  { id : PlayerId
+  , name : String
+  , level : Int
+  }
